1e60e1d9bfaaad3b225d5b8d177a5c2907a13c2f
henryhe1234 <henryhe1123@gmail.com>
2021-02-20 11:40:49 -0700

    practice props in react

--- example/components/src/index.js
+++ example/components/src/index.js
@@ -6,9 +6,26 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        content="Good day today!"
+        image = {faker.image.image()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        content="please fight"
+        image = {faker.image.image()}
+        
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        content="You can do this"
+        image = {faker.image.image()}
 
+      />
     </div>
   );
 };
