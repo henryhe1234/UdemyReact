b76176715025cc7d4bd5081513a08671f963cb3f
henryhe1234 <henryhe1123@gmail.com>
2021-02-20 22:54:03 -0700

    add spinner and defaultProps

--- example/seasons/src/index.js
+++ example/seasons/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay"
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   
@@ -23,9 +25,9 @@ class App extends React.Component {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     } else if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude:{this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />
     } else {
-      return <div>Loading</div>;
+      return <Spinner message="Please accept location request"/>;
     }
   }
 }
