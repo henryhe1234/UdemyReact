cd6eca740c62fc04a2c5382d45d66840432ac4e3
henryhe1234 <henryhe1123@gmail.com>
2021-02-20 21:23:31 -0700

    remove constructor, just using state, also add lifecycle method componenetDidMount

--- example/seasons/src/index.js
+++ example/seasons/src/index.js
@@ -2,22 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  
 
-    this.state = { lat: null };
+	state = {lat:null,errorMessage:""};
+
+  componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-				//we called setState!
-				this.setState({lat:position.coords.latitude })
-			},
-      (error) => console.log(error)
+        //we called setState!
+        this.setState({ lat: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
     );
   }
 
   //react says we have to define render!!
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude:{this.state.lat}</div>;
+    } else {
+      return <div>Loading</div>;
+    }
   }
 }
 
