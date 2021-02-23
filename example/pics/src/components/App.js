ab3568e0a46f26c332a2eaa1e062bfb8ea1c9cb3
henryhe1234 <henryhe1123@gmail.com>
2021-02-22 23:11:03 -0700

    create video app

--- example/pics/src/components/App.js
+++ example/pics/src/components/App.js
@@ -1,29 +1,56 @@
+// import React from "react";
+// import SearchBar from "./SearchBar";
+// import unsplash from "../api/unsplash";
+// import ImageList from "./ImageList";
+
+// class App extends React.Component {
+//   state = {images: [] };
+
+//   onSearchSubmit = async (term) => {
+//     const response = await unsplash
+//       .get("/search/photos", {
+//         params: { query: term }
+
+//       })
+
+//       this.setState({images: response.data.results});
+//   }
+
+//   render() {
+//     return (
+//       <div className="ui container" style={{ marginTop: "10px" }}>
+//         <SearchBar onSubmit={this.onSearchSubmit} />
+//         <ImageList images={this.state.images} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
 import React from "react";
 import SearchBar from "./SearchBar";
-import unsplash from "../api/unsplash";
-import ImageList from "./ImageList";
-
+import youtube from "../api/youtube"
+import VideoList from "./VideoList"
 class App extends React.Component {
-  state = {images: [] };
+  state = {videos:[]}
 
-  onSearchSubmit = async (term) => {
-    const response = await unsplash
-      .get("/search/photos", {
-        params: { query: term }
-        
-      })
-      
-      this.setState({images: response.data.results});
+  onTermSubmit = async (term)=>{
+    const response = await youtube.get('/search',{
+      params:{
+        q:term
+      }
+    })
+    this.setState({videos:response.data.items})
+    
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList images={this.state.images} />
+        <SearchBar onTermSubmit={this.onTermSubmit}/>
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
 }
-
 export default App;
--- example/pics/src/components/SearchBar.js
+++ example/pics/src/components/SearchBar.js
@@ -1,31 +1,61 @@
+// import React from "react";
+
+// class SearchBar extends React.Component {
+//   state = { term: "" };
+//   // constructor(){
+//   //   super()
+//   //   this.onFormSubmit = this.onFormSubmit.bind(this)
+//   // }
+
+//   // onInputClick(){
+//   //   console.log('Input was clicked')
+//   // }
+
+//   onFormSubmit = (event)=>{
+//     event.preventDefault();
+
+//     this.props.onSubmit(this.state.term);
+//   }
+
+//   render() {
+//     return (
+//       <div className="ui segment">
+//         <form onSubmit={this.onFormSubmit} className="ui form">
+//           <div className="field">
+//             <label>Image Search</label>
+//             <input
+//               type="text"
+//               value={this.state.term}
+//               onChange={(e) => this.setState({ term: e.target.value })}
+//             />
+//           </div>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// export default SearchBar;
 import React from "react";
 
 class SearchBar extends React.Component {
   state = { term: "" };
-  // constructor(){
-  //   super()
-  //   this.onFormSubmit = this.onFormSubmit.bind(this)
-  // }
 
-  // onInputClick(){
-  //   console.log('Input was clicked')
-  // }
-
-  onFormSubmit = (event)=>{
-    event.preventDefault();
-
-    this.props.onSubmit(this.state.term);
+  onFormSubmit = (e)=>{
+    e.preventDefault();
+    this.props.onTermSubmit(this.state.term)
+    //make sure we call callback from parent component
   }
 
   render() {
     return (
-      <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
-            <label>Image Search</label>
+            <label>Video Search</label>
             <input
               type="text"
-              value={this.state.term}              
+              value={this.state.term}
               onChange={(e) => this.setState({ term: e.target.value })}
             />
           </div>
