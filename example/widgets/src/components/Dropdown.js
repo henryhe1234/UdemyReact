8846a48f73606f31a0dbdd2e43d5b342b86bec90
henryhe1234 <henryhe1123@gmail.com>
2021-02-25 23:56:39 -0700

    finish google translate api

--- example/widgets/src/components/Dropdown.js
+++ example/widgets/src/components/Dropdown.js
@@ -1,13 +1,27 @@
-import React,{useState} from "react";
+import React,{useState,useEffect,useRef} from "react";
 
-const Dropdown = ({ options,selected,onSelectedChange }) => {
+const Dropdown = ({ label, options,selected,onSelectedChange }) => {
   const [open,setOpen] = useState(false)
+  const ref = useRef();
+  useEffect(()=>{
+    const onBodyClick = (event)=>{
+      if(ref.current && ref.current.contains(event.target)){
+        return; 
+      }
+      setOpen(false)
+    }
+    document.body.addEventListener('click',onBodyClick,{capture:true})
+
+    return ()=>{
+      document.body.removeEventListener('click',onBodyClick)
+    }
+  },[])
 
   const renderOptions = options.map((option) => {
     if(selected.value === option.value){
       return null;
     }
-
+    
     return (
       <div key={option.value} className="item" onClick={()=>onSelectedChange(option)}>
         {option.label}
@@ -17,9 +31,9 @@ const Dropdown = ({ options,selected,onSelectedChange }) => {
 
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
-        <label className="label">Select a Color</label>
+        <label className="label">{label}</label>
         <div onClick={()=>setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
           <i className="dropdown icon"></i>
           <div className="text">
